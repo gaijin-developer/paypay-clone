85fbe99c004d13380d8c11afc1ef5699c742fb59
Frank Kojo Entsie <frank.entsie301@gmail.com>
2024-03-06 10:29:36 +0900

    the home screen setup has been added and prepared and ready to start building

--- App.tsx
+++ App.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
-// import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 import Header from './components/Header/Header';
 
 function App(): React.JSX.Element {
-  // const isDarkMode = useColorScheme() === 'dark';
-
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // };
-
   return (
     <SafeAreaView>
-      <View>
-        <Header />
-      </View>
-      <View>
-        
-      </View>
+      <ScrollView>
+        <View>
+          <Header />
+        </View>
+
+        <View>
+          <Text> Scanner </Text>
+        </View>
+        <View>
+          <Text> Immediate Use </Text>
+        </View>
+        <View>
+          <Text> Collection </Text>
+        </View>
+
+        <View>
+          <Text> Horizontal Promos </Text>
+        </View>
+
+        <View>
+          <Text> Coupons </Text>
+        </View>
+        <View>
+          <Text> Recommendationa </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
